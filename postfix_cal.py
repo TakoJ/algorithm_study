7d8127fdbc2149d4bbe6c711af690596c709988b
TakoJ <hgy3047@gmail.com>
2018-12-14 15:50:16 +0900

    후위표기법을 이용한 괄호를 포함한 사칙연산

--- postfix_cal.py
+++ postfix_cal.py
@@ -1,11 +1,12 @@
-## 후위표기법을 이용하여 사칙연산하기
+# 후위표기법을 이용하여 사칙연산하기
 
-infix = ['(', '2', '+', '3', ')', '*', '4']
+infix = ['(', '2', '+', '3', ')', '*', '4', '/', '2']
 postfix = []
 stack = []
 result = []
 operator = ['*', '/', '+', '-']
 bracket = ['(', ')']
+
 
 def is_number(x):
     if x not in operator and x not in bracket:
@@ -15,35 +16,13 @@ def is_number(x):
 
 
 def pref(x):
-    if x is '*' or '/':
+    if x is '*' or x is '/':
         return 1
-    elif x is '+' or '-':
+    elif x is '+' or x is '-' or x is '(' or x is ')':
         return 0
 
 
-def calc_two_oprd(oprd1, oprd2, oprt):
-    if oprt == '+':
-        return oprd1 + oprd2
-    elif oprt == '-':
-        return oprd1 - oprd2
-    elif oprt == '*':
-        return oprd1 * oprd2
-    elif oprt == '/':
-        return oprd1 // oprd2
-
-
-def calc(post):
-    for p in post:
-        if is_number(p):
-            result.append(int(p))
-        else:
-            oprd2 = result.pop()  # 먼저 뽑은걸 2로.
-            oprd1 = result.pop()
-            result.append(calc_two_oprd(oprd2, oprd1, p))
-    return result.pop()
-
-
-def main():
+def make_postfix(infix):
     for c in infix:
         if is_number(c):
             postfix.append(c)
@@ -68,6 +47,33 @@ def main():
     while len(stack) > 0:
         postfix.append(stack.pop())
 
+    return postfix
+
+
+def calc_two_oprd(oprd1, oprd2, oprt):
+    if oprt == '+':
+        return oprd1 + oprd2
+    elif oprt == '-':
+        return oprd1 - oprd2
+    elif oprt == '*':
+        return oprd1 * oprd2
+    elif oprt == '/':
+        return oprd1 // oprd2
+
+
+def calc(post):
+    for p in post:
+        if is_number(p):
+            result.append(int(p))
+        else:
+            oprd1 = result.pop()
+            oprd2 = result.pop()
+            result.append(calc_two_oprd(oprd2, oprd1, p))
+    return result.pop()
+
+
+def main():
+    postfix = make_postfix(infix)
     print("{} 의 결과는 {}".format(postfix, calc(postfix)))
 
 
